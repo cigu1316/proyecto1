37e0b5ff2164321f1415d346a2e4a59a8cee9ed5
gustavo <cigu1966@gamil.com>
2022-12-28 23:55:10 -0300

    28/12-23.55

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -1,9 +1,17 @@
 
 from django.contrib import admin
 from django.urls import path
-from proyecto.views import hola_mundo
-
+from proyecto.views import hola_mundo ,otra_mas,fecha_actual,vista_con_edad,vista_con_template\
+                            
+                            
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hola/',hola_mundo)
+    path('hola/',hola_mundo ,name = 'hola_mundo'),
+    path('otra/',otra_mas),
+    path('fecha/',fecha_actual),
+    path('edad/<int:edad>/',vista_con_edad),
+    path('vista-con-template/',vista_con_template)
+    
+    
+    
 ]
--- proyecto/views.py
+++ proyecto/views.py
@@ -1,5 +1,19 @@
 from django.http import HttpResponse
+from datetime import datetime
+from django.shortcuts import render
 
 def hola_mundo(request):
     return HttpResponse('hola_mundo')
 
+def otra_mas(request):
+    return HttpResponse('si otras mas')
+
+def fecha_actual(request):
+    hoy = datetime.now().date()
+    return HttpResponse(f'la fecha de hoy es {hoy}')
+
+def vista_con_edad(request,edad):
+    return HttpResponse(f'¿esto funciona?la edad es {edad} años')
+
+def vista_con_template(request):
+    return render(request,'template.html', context={})
